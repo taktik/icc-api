4d787e6ee7b489a30726d52b22e9524487290005
Geoffroy Piroux <geoffroy.piroux@b12-consulting.com>
2018-10-18 13:52:19 +0200

    Revert "fix patient gender"
    This reverts commit 0019af9fe0003c1f0e6306d0738e6bf00eb96323.

--- icc-x-api/utils/fhc-patient-util.ts
+++ icc-x-api/utils/fhc-patient-util.ts
@@ -1,10 +1,9 @@
-import { PatientDto, HealthcarePartyDto } from "../../icc-api/model/models"
+import { PatientDto } from "../../icc-api/model/models"
 import { Patient } from "fhc-api/dist/model/models"
 import * as _ from "lodash"
 
 export function toPatient(patientDto: PatientDto): Patient {
   const patient = new Patient(patientDto)
-  patient.gender = mapGender(patientDto.gender)
   _.each(patient.insurabilities, ins => {
     ins.parameters = _.pick(ins.parameters, [
       "status",
@@ -16,22 +15,4 @@ export function toPatient(patientDto: PatientDto): Patient {
     ]) as { [key: string]: string }
   })
   return patient
-}
-
-function mapGender(gender: string): Patient.GenderEnum {
-  // Male = <any>"male",
-  // Female = <any>"female",
-  // Unknown = <any>"unknown",
-  // Indeterminate = <any>"indeterminate",
-  // Changed = <any>"changed",
-  // ChangedToMale = <any>"changedToMale",
-  // ChangedToFemale = <any>"changedToFemale"
-  switch (gender) {
-    case "male":
-      return Patient.GenderEnum.M
-    case "female":
-      return Patient.GenderEnum.F
-    default:
-      return Patient.GenderEnum.M
-  }
 }
