0397b4eddd7994ee73d7bd5924945b6a83da6bc5
Adrien Martorana <Adrien.Martorana@nsi-sa.be>
2018-11-02 19:04:17 +0100

    Adapt referring physician

--- icc-api/model/PatientHealthCarePartyDto.ts
+++ icc-api/model/PatientHealthCarePartyDto.ts
@@ -40,6 +40,7 @@ export class PatientHealthCarePartyDto {
 }
 export namespace PatientHealthCarePartyDto {
   export enum TypeEnum {
+    ReferringPhysician = <any>"referringphysician",
     Doctor = <any>"doctor",
     Referral = <any>"referral",
     Medicalhouse = <any>"medicalhouse",
