b01aaaa2236c215de978a6163d42f3dc6ff14504
Bogdan Safta <bsafta.meyvn@gmail.com>
2019-12-13 18:54:32 +0200

    MS-2047 Patch treatment frequencies after decryption

--- icc-x-api/icc-bekmehr-x-api.ts
+++ icc-x-api/icc-bekmehr-x-api.ts
@@ -4,6 +4,7 @@ import { XHR } from "../icc-api/api/XHR"
 import * as models from "../icc-api/model/models"
 import { IccContactXApi } from "./icc-contact-x-api"
 import { IccHelementXApi } from "./icc-helement-x-api"
+import { ContactDto } from "../icc-api/model/models"
 
 export class IccBekmehrXApi extends iccBeKmehrApi {
   private readonly ctcApi: IccContactXApi
@@ -35,7 +36,8 @@ export class IccBekmehrXApi extends iccBeKmehrApi {
     healthcarePartyId: string,
     resolve: (value?: Promise<Blob>) => void,
     reject: (reason?: any) => void,
-    progressCallback?: (progress: number) => void
+    progressCallback?: (progress: number) => void,
+    patcher?: (response: any[]) => Promise<any[]>
   ) {
     const that = this
     return (event: MessageEvent) => {
@@ -62,6 +64,7 @@ export class IccBekmehrXApi extends iccBeKmehrApi {
           } else {
             that.ctcApi
               .decryptServices(healthcarePartyId, msg.body)
+              .then(res => (patcher ? patcher(res) : Promise.resolve(res)))
               .then(res =>
                 socket.send(
                   JSON.stringify({ command: "decryptResponse", uuid: msg.uuid, body: res })
@@ -184,6 +187,7 @@ export class IccBekmehrXApi extends iccBeKmehrApi {
     language: string,
     version: number,
     body: models.MedicationSchemeExportInfoDto,
+    patcher: (response: ContactDto[]) => Promise<ContactDto[]>,
     sessionId?: string
   ): Promise<Blob> {
     return new Promise((resolve, reject) => {
@@ -202,7 +206,7 @@ export class IccBekmehrXApi extends iccBeKmehrApi {
       // Listen for messages
       socket.addEventListener(
         "message",
-        this.socketEventListener(socket, healthcarePartyId, resolve, reject)
+        this.socketEventListener(socket, healthcarePartyId, resolve, reject, undefined, patcher)
       )
     })
   }
