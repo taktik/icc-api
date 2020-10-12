9829ec8f225a77063348971e2089e1f571886eaa
Antoine Duchâteau <ad@taktik.com>
2020-10-12 11:06:45 +0200

    Test for creating a patient

--- test/cryptoTest.ts
+++ test/cryptoTest.ts
@@ -55,13 +55,15 @@ describe("Create a patient from scratch", () => {
 
       const patient = await api.patienticc.createPatientWithUser(
         user,
-        new PatientDto({
-          id: api.cryptoicc.randomUuid(),
-          lastName: "Biden",
-          firstName: "Joe",
-          note: "A secured note that is encrypted"
-        })
-      ) //curl -X POST "https://kraken.svc.icure.cloud/rest/v1/patient" -u abdemo:knalou -H  "Content-Type: application/json" -d '{"id":"e4ca63b6-f9bf-4b7d-802d-d704394dde95","lastName":"Biden","firstName":"Joe",note:"A secured note that is encrypted", "delegations":{...}, "encryptedKeys":{...}, "encryptedSelf":"..."'
+        await api.patienticc.newInstance(
+          user,
+          new PatientDto({
+            lastName: "Biden",
+            firstName: "Joe",
+            note: "A secured note that is encrypted"
+          })
+        )
+      ) //curl -X POST "https://kraken.svc.icure.cloud/rest/v1/patient" -u abdemo:knalou -H  "Content-Type: application/json" -d '{"id":"...","lastName":"Biden","firstName":"Joe",note:"A secured note that is encrypted", "delegations":{...}, "encryptedKeys":{...}, "encryptedSelf":"..."'
 
       console.log(
         `Created patient (decrypted): ${patient.id}: ${patient.firstName} ${
