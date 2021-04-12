1f80fe2d8723ae23b8334cabd85af430e5256b3e
Antoine Duchâteau <ad@taktik.com>
2021-04-12 13:31:16 +0200

    ICURE-186 #time 1h Added patient filters

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ export * from "./icc-api"
 export * from "./icc-api/model/models"
 
 export * from "./icc-x-api/utils/binary-utils"
+export * from "./icc-x-api/filters/filters"
 
 export { XHR } from "./icc-api/api/XHR"
