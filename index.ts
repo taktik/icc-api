4f6da6e03d3669d9d318c6081704789e459efcfc
Antoine Duchâteau <ad@taktik.com>
2021-04-12 10:25:49 +0200

    Simplify exports

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 export * from "./icc-x-api"
 export * from "./icc-api"
 export * from "./icc-api/model/models"
+
+export * from "./icc-x-api/utils/binary-utils"
+
 export { XHR } from "./icc-api/api/XHR"
