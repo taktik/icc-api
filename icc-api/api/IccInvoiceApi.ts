8c7795c9dc06b6cdbee90fe3392637874ac89dd8
Antoine Duchâteau <ad@taktik.com>
2021-07-12 14:22:40 +0200

    Add new Batch calls to invoice

--- icc-api/api/IccInvoiceApi.ts
+++ icc-api/api/IccInvoiceApi.ts
@@ -100,6 +100,23 @@ export class IccInvoiceApi {
   }
 
   /**
+   * Returns the created invoices.
+   * @summary Create a batch of invoices
+   * @param body
+   */
+  createInvoices(body?: Array<Invoice>): Promise<Array<Invoice>> {
+    let _body = null
+    _body = body
+
+    const _url = this.host + `/invoice/batch` + '?ts=' + new Date().getTime()
+    let headers = this.headers
+    headers = headers.filter((h) => h.header !== 'Content-Type').concat(new XHR.Header('Content-Type', 'application/json'))
+    return XHR.sendCommand('POST', _url, headers, _body, this.fetchImpl)
+      .then((doc) => (doc.body as Array<JSON>).map((it) => new Invoice(it)))
+      .catch((err) => this.handleError(err))
+  }
+
+  /**
    *
    * @summary Deletes an invoice
    * @param invoiceId
@@ -528,6 +545,23 @@ export class IccInvoiceApi {
     headers = headers.filter((h) => h.header !== 'Content-Type').concat(new XHR.Header('Content-Type', 'application/json'))
     return XHR.sendCommand('PUT', _url, headers, _body, this.fetchImpl)
       .then((doc) => new Invoice(doc.body as JSON))
+      .catch((err) => this.handleError(err))
+  }
+
+  /**
+   * Returns the modified invoices.
+   * @summary Modify a batch of invoices
+   * @param body
+   */
+  modifyInvoices(body?: Array<Invoice>): Promise<Array<Invoice>> {
+    let _body = null
+    _body = body
+
+    const _url = this.host + `/invoice/batch` + '?ts=' + new Date().getTime()
+    let headers = this.headers
+    headers = headers.filter((h) => h.header !== 'Content-Type').concat(new XHR.Header('Content-Type', 'application/json'))
+    return XHR.sendCommand('PUT', _url, headers, _body, this.fetchImpl)
+      .then((doc) => (doc.body as Array<JSON>).map((it) => new Invoice(it)))
       .catch((err) => this.handleError(err))
   }
 
